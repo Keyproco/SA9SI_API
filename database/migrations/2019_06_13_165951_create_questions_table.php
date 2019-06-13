732fe291fb08b15d12f1fa6240545cfea97dde57
Oussama Keddar <oussama.keddar@epitech.eu>
2019-06-13 19:14:44 +0200

    fill the migrate schema with initial fields

--- database/migrations/2019_06_13_165951_create_questions_table.php
+++ database/migrations/2019_06_13_165951_create_questions_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateQuestionsTable extends Migration
 {
@@ -15,6 +15,9 @@ class CreateQuestionsTable extends Migration
     {
         Schema::create('questions', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->unsignedInteger('user_id');
+            $table->string('title');
+            $table->string('body');
             $table->timestamps();
         });
     }
