8c264615cd4267d34f6f653051272542523f23d8
Oussama Keddar <oussama.keddar@epitech.eu>
2019-06-13 19:31:59 +0200

    Create a factory for Questions
    Create 10 rows of questions with random user_id number

---
+++ database/factories/Question.php
@@ -0,0 +1,16 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Question;
+use Faker\Generator as Faker;
+
+$factory->define(Question::class, function (Faker $faker) {
+    return [
+        'title' => $faker->sentence,
+        'body' => $faker->sentence,
+        'user_id' => $faker->numberBetween(0, 5),
+        'updated_at' => now(),
+        'created_at' => now(),
+    ];
+});
