cfd9738d537387168c988dd2dd98f089105e03e0
Oussama Keddar <oussama.keddar@epitech.eu>
2019-06-19 23:19:18 +0200

    Create Api login Controller

---
+++ app/Http/Controllers/Api/Auth/LoginController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class LoginController extends Controller
+{
+    public function login()
+    { }
+}
