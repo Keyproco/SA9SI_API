002bbd3d7b92e9374b1c8aebad32b1d3b24ec189
Oussama Keddar <oussama.keddar@epitech.eu>
2019-06-21 00:42:44 +0200

    Add route and logic to Api\loginController

--- app/Http/Controllers/Api/Auth/LoginController.php
+++ app/Http/Controllers/Api/Auth/LoginController.php
@@ -2,11 +2,27 @@
 
 namespace App\Http\Controllers\Api\Auth;
 
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\User;
 
 class LoginController extends Controller
 {
-    public function login()
-    { }
+    public function login(Request $request)
+    {
+        $this->validate($request, [
+            'username' => 'required',
+            'password' => 'required'
+        ]);
+        $user = User::where('email', $request->username)->first();
+        if (!$user) {
+            return response(['status' => 'Error', 'message' => 'User not found']);
+        }
+        if (Hash::check($request->password, $user->password)) {
+            return response(['status' => 'Success', 'acess_token' =>  $user->createToken('authToken')->accessToken]);
+        } else {
+            return response(['status' => 'Error', 'message' => 'Wrong password']);
+        }
+    }
 }
