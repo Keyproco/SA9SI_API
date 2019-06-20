98cb2fc20f99e1c4639247e6092b343c2176eee8
Oussama Keddar <oussama.keddar@epitech.eu>
2019-06-20 12:25:13 +0200

    Update Api UserController, returns accessToken

--- app/Http/Controllers/Api/Auth/RegisterController.php
+++ app/Http/Controllers/Api/Auth/RegisterController.php
@@ -2,13 +2,36 @@
 
 namespace App\Http\Controllers\Api\Auth;
 
+use Laravel\Passport\Client;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Support\Facades\Route;
 
 class RegisterController extends Controller
 {
+    private $client;
+    public function __construct()
+    {
+        $this->client = Client::find(1);
+    }
     public function register(Request $request)
     {
-        return 'Api register controller';
+        $this->validate(
+            $request,
+            [
+                'name' => 'required',
+                'email' => 'required|email|unique:users,email',
+                'password' => 'required|min:6|confirmed',
+            ]
+        );
+
+        $user = User::create([
+            'name' => request('name'),
+            'email' => request('email'),
+            'password' => bcrypt(request('password')),
+        ]);
+        $accessToken = $user->createToken('authToken')->accessToken;
+        return response(['user' => $user, 'access_token' => $accessToken]);
     }
 }
