05e55d73728548333625b8d3d88453313ddbff46
Keyproco <oussama.keddar@epitech.eu>
2019-10-13 17:43:02 +0200

    add react router dom

--- resources/js/index.js
+++ resources/js/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom';
 // import Axios from "axios";
 import styled from 'styled-components';
 import Header from './components/Header';
+import { BrowserRouter } from 'react-router-dom';
 export default class App extends Component {
 	componentWillMount() {}
 	render() {
-		return <Header />;
+		return (
+			<BrowserRouter>
+				<Header />
+			</BrowserRouter>
+		);
 	}
 }
 
